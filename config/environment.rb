0184a1a50434fa600246338681c4695146ef78cd
Xavier Canal i Masjuan <xeviknal@gmail.com>
2017-08-15 19:52:45 +0200

    Connect ActiveRecord to a database

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@ require 'active_support'
 require 'active_record'
 
 Dir['./lib/**/*.rb'].each { |f| require f }
+Dir['./app/models/*.rb'].each { |f| require f }
 
 module MusicLibrary
   class Application
@@ -10,6 +11,7 @@ module MusicLibrary
 
     def initialize
       @router = Router.new
+      establish_connection
     end
 
     def call(env)
@@ -22,6 +24,18 @@ module MusicLibrary
       else
         Response.not_found
       end.to_rack
+    end
+
+    private
+
+    def establish_connection
+      database_config = YAML.load_file("./config/database.yml")
+      ActiveRecord::Base.establish_connection(
+        adapter:  database_config["adapter"],
+        database: "db/#{database_config["database"]}.sqlite3",
+        pool:     database_config["pool"],
+        timeout:  database_config["timeout"]
+      )
     end
   end
 end
