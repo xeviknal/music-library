4cd7d1237914d3fcf44d5eff3b95eaafca58d690
Xavier Canal i Masjuan <xeviknal@gmail.com>
2017-08-17 21:32:47 +0200

    Adding database cleaner to spec suite

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 require 'pry'
+require 'database_cleaner'
 
 ENV['MUSIC_ENV'] ||= 'test'
 
@@ -8,3 +9,16 @@ MusicLibraryApp = MusicLibrary::Application.new
 Dir['./config/*.rb'].each { |f| require f }
 Dir['./lib/*.rb'].each { |f| require f }
 Dir['./spec/support/*.rb'].each { |f| require f }
+
+RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+end
